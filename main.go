1c43e6b18caeef33552472451e572fc0b31e612f
rahmaas <ash.rahman@verizonwireless.com>
2018-10-03 15:29:59 -0500

    serve index.html file

--- main.go
+++ main.go
@@ -6,14 +6,17 @@ import (
 	"github.com/go-chi/chi"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"status": "You can now get started on your app!"}`))
+}
+
 func main() {
 	r := chi.NewRouter()
-	
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"status": "You can now get started on your app!"}`))
-	})
+
+	r.Handle("/", http.FileServer(http.Dir("./static")))
+	r.Get("/example", handler)
 
 	fmt.Println("Your app is now serving at 127.0.0.1:3000")
 
